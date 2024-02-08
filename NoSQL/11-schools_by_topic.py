538e1e7c2beefdb3f89a09f9fe22e81905ce6908
gerberth92 <gerberthcontreras@gmail.com>
2024-02-07 21:35:24 -0500

    corrigiendo el nombre

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,4 +5,4 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """ function that returns the list of school having a specific topic"""
-    return (list(mongo_collection.find({'topic': topic})))
+    return (list(mongo_collection.find({'topics': topic})))
