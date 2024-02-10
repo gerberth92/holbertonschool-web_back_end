7973d6b7107639f285c1689c8f440da9f894c4e7
gerberth92 <gerberthcontreras@gmail.com>
2024-02-10 12:57:52 -0500

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str that takes a float n as argument and returns
+    the string representation of the float.
+
+    Args:
+        n(float): argumento.
+
+    Return:
+        str: n.
+    """
+    return (str(n))
