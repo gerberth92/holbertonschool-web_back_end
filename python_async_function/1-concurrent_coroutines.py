6612a7892c0cdaaca42da13b378eb6ca6b115e55
gerberth92 <gerberthcontreras@gmail.com>
2024-03-09 13:35:29 -0500

    asyncio.gather

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Let's execute multiple coroutines at the same time with async."""
+
+import asyncio
+import random
+from typing import List
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """corutina"""
+
+    valor = random.uniform(0, max_delay)
+    await asyncio.sleep(valor)
+    return (valor)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """concurrecia"""
+
+    task_list = []
+    for i in range(n):
+        tarea = await asyncio.gather(wait_random(max_delay))
+        for i in tarea:
+            task_list.append(i)
+    return (task_list)
