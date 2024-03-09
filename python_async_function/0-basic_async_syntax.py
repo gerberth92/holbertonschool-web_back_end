1e8b40ecc218bb00f6efb6167261c5ae3964b1ed
gerberth92 <gerberthcontreras@gmail.com>
2024-03-08 22:41:16 -0500

    comment

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,7 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """Funcion que retorna un float"""
     valor = random.uniform(0, max_delay)
     await asyncio.sleep(valor)
     return (valor)
