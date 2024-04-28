1b859fc3349a0cadc734e6520ac325a6ca50fe98
gerberth92 <gerberthcontreras@gmail.com>
2024-04-28 12:45:25 -0500

    str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Este modulo tiene anotaciones. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Esta funcion concatena dos strings """
+
+    return (str1 + str2)
