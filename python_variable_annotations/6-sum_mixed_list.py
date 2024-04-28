26d444c656bc45ca22069dfac46f8e5ea9352580
gerberth92 <gerberthcontreras@gmail.com>
2024-04-28 15:06:03 -0500

    Union

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" Este modulo tiene anotaciones. """
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Esta funcion combina datos. """
+
+    return (sum(mxd_lst))
