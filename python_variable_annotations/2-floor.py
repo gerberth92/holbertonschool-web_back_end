5bf7965297adac867c6d2385c7eaa5295bf8a23a
gerberth92 <gerberthcontreras@gmail.com>
2024-04-28 12:55:08 -0500

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Este modulo tiene anotaciones. """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ Esta funcion redondea hacia abajo. """
+
+    return (math.floor(n))
