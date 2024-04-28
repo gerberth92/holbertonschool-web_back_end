3784f3f30696f2e3196ba223d85d22ad1786bbce
gerberth92 <gerberthcontreras@gmail.com>
2024-04-28 14:59:59 -0500

    List

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Este modulo tiene anotaciones. """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Esta funcion esta anotada. """
+
+    return (sum(input_list))
