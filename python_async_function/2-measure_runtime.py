5db40afc160d78fadc1d3a3f2626b112db17107c
gerberth92 <gerberthcontreras@gmail.com>
2024-04-28 21:15:51 -0500

    task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Este modulo mide el tiempo de ejecucion. """
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ Esta funcion mide el tiempo de ejecucion. """
+
+    inicio = time.time()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    fin = time.time()
+
+    return ((fin - inicio) / n)
