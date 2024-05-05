2caa524079f888f730312b66b8a4139674074837
gerberth92 <gerberthcontreras@gmail.com>
2024-05-04 21:08:31 -0500

    task12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Este modulo tiene una funcion mongo. """
+
+from pymongo import MongoClient
+
+
+def status():
+    """ Muestra los stados de consulta. """
+
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs
+    nginx = logs.nginx
+
+    metodos = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(nginx.count(), "logs")
+
+    print("Methods:")
+
+    for metodo in metodos:
+        print("\tmethod {}: {}"
+              .format(metodo, nginx.count({'method': metodo})))
+
+    print("{} status check"
+          .format(nginx.count({'method': 'GET', 'path': '/status'})))
