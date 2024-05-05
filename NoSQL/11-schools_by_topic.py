d40704cf9f3f837c11719cf5698692622bbd4ccb
gerberth92 <gerberthcontreras@gmail.com>
2024-05-04 20:31:41 -0500

    task11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Este modulo tiene una funcion mongo. """
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ retorna una lista de un documento en especifico. """
+
+    retorno = []
+
+    docs = mongo_collection.find({'topic': topic})
+
+    for doc in docs:
+        retorno.append(doc)
+
+    return (retorno)
