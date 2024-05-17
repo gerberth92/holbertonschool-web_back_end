693f97061ac7ade7b333aef1530f208414454e2b
gerberth92 <gerberthcontreras@gmail.com>
2024-05-17 16:04:27 -0500

    mejorando el style

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -11,36 +11,39 @@ export default class HolbertonCourse {
     }
     this._name = name;
     this._length = length;
-    this._students = students
+    this._students = students;
   }
 
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw TypeError('name debe de ser un string');
     }
-    return this._name = name;
+    this._name = name;
   }
 
   get length() {
     return this._length;
   }
+
   set length(length) {
     if (typeof length !== 'number') {
       throw TypeError('length debe de ser numero');
     }
-    return this._length = length;
+    this._length = length;
   }
 
   get students() {
     return this._students;
   }
+
   set students(students) {
     if (!Array.isArray(students)) {
       throw TypeError('students debe de ser un array');
     }
-    return this._students = students;
+    this._students = students;
   }
 }
