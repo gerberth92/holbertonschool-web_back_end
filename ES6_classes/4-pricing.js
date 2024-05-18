c72892295499f713e127cd5ae3d30f0c2102b040
gerberth92 <gerberthcontreras@gmail.com>
2024-05-18 09:45:46 -0500

    corrigiendo style

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,5 +1,3 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
@@ -9,22 +7,24 @@ export default class Pricing {
   get amount() {
     return this._amount;
   }
+
   set amount(amount) {
-    return this._amount = amount;
+    this._amount = amount;
   }
 
   get currency() {
     return this._currency;
   }
+
   set currency(currency) {
-    return this._currency = currency;
+    this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency._name} (${this._currency._code})`
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate
+    return amount * conversionRate;
   }
 }
