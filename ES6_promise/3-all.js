de79d9303b03daa93b6f52ea994c103695a7b00f
gerberth92 <gerberthcontreras@gmail.com>
2024-05-26 14:53:43 -0500

    add return

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const foto = uploadPhoto();
   const user = createUser();
 
-  Promise.all([foto, user])
+  return Promise.all([foto, user])
     .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
