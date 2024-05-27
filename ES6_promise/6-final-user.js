d04d98dda6bf609b2fa8eec3279b0aadb1428efb
gerberth92 <gerberthcontreras@gmail.com>
2024-05-26 21:35:48 -0500

    add async

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promesas = [signUpUser(fileName, lastName), uploadPhoto(fileName)];
 
-  return Promise.allSettled(promesas)
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  const results = await Promise.allSettled(promesas);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
